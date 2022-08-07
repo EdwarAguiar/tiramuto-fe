520e32281bfade81b59a14770990b23e3d9a512f
Edwar Aguiar <Aguiar.Edwar@gmail.com>
2022-08-07 12:59:45 -0500

    [3] Se instalaron las herramientas ESLint y Prettier

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,9 +4,9 @@ import '../styles/components/App.css';
 const App = () => {
   return (
     <div>
-        <h1>Hola Tiramuto!!!</h1>
+      <h1>Hola Tiramuto!!!</h1>
     </div>
   );
-}
+};
 
 export default App;
