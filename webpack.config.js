92b1f21e780cbbd7200f0669f16a0c5f7491152c
Edwar Aguiar <Aguiar.Edwar@gmail.com>
2022-08-07 07:33:48 -0500

    [2] Se instalo css loaders y plugins necesarios para los estilos

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -28,6 +29,15 @@ module.exports = {
                     },
                 ],
             },
+            {
+                test: /\.css$/,
+                use: [
+                  {
+                    loader: MiniCssExtractPlugin.loader,
+                  },
+                  'css-loader',
+                ],
+              },
         ],
     },
     plugins: [
@@ -35,6 +45,9 @@ module.exports = {
           template: './public/index.html',
           filename: './index.html',
         }),
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].css',
+          }),        
     ],
     devServer: {
         static: { 
