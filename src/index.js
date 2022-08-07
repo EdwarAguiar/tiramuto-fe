5a4f9fed4b7724622c2bbe126ff8b0c27326b5b7
Edwar Aguiar <Aguiar.Edwar@gmail.com>
2022-08-07 07:02:52 -0500

    [1] Hola Tiramuto - Aplicacion minima

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+// Version vieja de React 17
+// ReactDOM.render(<App />, document.getElementById('app'));
+
+// React 18 usa esta forma
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
