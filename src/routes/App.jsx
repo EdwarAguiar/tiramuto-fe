63913eca355c30d2c2ddd2f309dc9de9817fd698
Edwar Aguiar <Aguiar.Edwar@gmail.com>
2022-08-07 20:01:29 -0500

    [6] Se crea el Layout y agregan Estilos

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+
 import Home from '../containers/Home';
 import Store from '../containers/Store';
 import Blog from '../containers/Blog';
@@ -10,21 +11,24 @@ import Information from '../containers/Information';
 import Payment from '../containers/Payment';
 import Success from '../containers/Success';
 import Notfound from '../containers/Notfound';
+import Layout from '../components/Layout';
 
 const App = () => {
     return (
         <BrowserRouter>
-        <Routes>
-            <Route path='/' element={ <Home />} />
-            <Route path='/store' element={ <Store />} />
-            <Route path='/blog' element={ <Blog />} />
-            <Route path='/apishow' element={ <Apishow />} />
-            <Route path='/checkout' element={<Checkout />} />
-            <Route path='/checkout/information' element={<Information />} />
-            <Route path='/checkout/payment' element={<Payment />} />
-            <Route path='/checkout/success' element={<Success />} />
-            <Route path='*' element={<Notfound />} />
-        </Routes>
+            <Layout>
+                <Routes>
+                    <Route path='/' element={ <Home />} />
+                    <Route path='/store' element={ <Store />} />
+                    <Route path='/blog' element={ <Blog />} />
+                    <Route path='/apishow' element={ <Apishow />} />
+                    <Route path='/checkout' element={<Checkout />} />
+                    <Route path='/checkout/information' element={<Information />} />
+                    <Route path='/checkout/payment' element={<Payment />} />
+                    <Route path='/checkout/success' element={<Success />} />
+                    <Route path='*' element={<Notfound />} />
+                </Routes>
+            </Layout>
         </BrowserRouter>
     );
 }
