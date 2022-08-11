004a34ebee1c90e94c759e9fb8b9d763def3ca03
Edwar Aguiar <Aguiar.Edwar@gmail.com>
2022-08-11 06:01:33 -0500

    [12] Se crea e implementa el CONTEXT de la Aplicacion en el Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import AppContext from '../context/AppContext';
 import '../styles/components/Header.css';
 
 const Header = () => {
+  const { state } = useContext(AppContext);
+  const { cart } = state;
+
   return (
     <div className="Header">
-        <h1 className="Header-title">
-          <Link to="/">Tiramuto Store</Link>
-        </h1>
-        <div className="Header-checkout">
+        <div className="Header-up">
+          <h1 className="Header-title"><Link to="/">Tiramuto Store</Link></h1>
+          <div className="Header-checkout">
             <Link to="/checkout">
-              <i className="fas fa-shopping-basket"></i>
+              <i className="fas fa-shopping-basket" />
             </Link>
+            {cart.length > 0 && <div className="Header-alert"> {cart.length} </div>}
+          </div>
+        </div>
+        <div className="Header-menu">
+           <h3><Link to="/">Home</Link></h3>
+           <h3><Link to="/aboutme">About Me</Link></h3>
+           <h3><Link to="/store">Store</Link></h3>
+           <h3><Link to="/blog">Blog</Link></h3>
+           <h3><Link to="/apishow">API Show Run</Link></h3>
         </div>
     </div>
   );
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-
 import Home from '../containers/Home';
 import Store from '../containers/Store';
 import Blog from '../containers/Blog';
@@ -12,24 +11,29 @@ import Payment from '../containers/Payment';
 import Success from '../containers/Success';
 import Notfound from '../containers/Notfound';
 import Layout from '../components/Layout';
+import AppContext from '../context/AppContext';
+import useInitialState from '../hooks/useInitialState';
 
 const App = () => {
+    const initialState = useInitialState();
     return (
-        <BrowserRouter>
-            <Layout>
-                <Routes>
-                    <Route path='/' element={ <Home />} />
-                    <Route path='/store' element={ <Store />} />
-                    <Route path='/blog' element={ <Blog />} />
-                    <Route path='/apishow' element={ <Apishow />} />
-                    <Route path='/checkout' element={<Checkout />} />
-                    <Route path='/checkout/information' element={<Information />} />
-                    <Route path='/checkout/payment' element={<Payment />} />
-                    <Route path='/checkout/success' element={<Success />} />
-                    <Route path='*' element={<Notfound />} />
-                </Routes>
-            </Layout>
-        </BrowserRouter>
+        <AppContext.Provider value={initialState}>
+            <BrowserRouter>
+                <Layout>
+                    <Routes>
+                        <Route path='/' element={ <Home />} />
+                        <Route path='/store' element={ <Store />} />
+                        <Route path='/blog' element={ <Blog />} />
+                        <Route path='/apishow' element={ <Apishow />} />
+                        <Route path='/checkout' element={<Checkout />} />
+                        <Route path='/checkout/information' element={<Information />} />
+                        <Route path='/checkout/payment' element={<Payment />} />
+                        <Route path='/checkout/success' element={<Success />} />
+                        <Route path='*' element={<Notfound />} />
+                    </Routes>
+                </Layout>
+            </BrowserRouter>
+        </AppContext.Provider>
     );
 }
 
