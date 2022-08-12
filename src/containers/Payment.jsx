ae24ddbc5400ba2764c1bc47daab72f0c6dcc7f7
Edwar Aguiar <Aguiar.Edwar@gmail.com>
2022-08-12 11:11:01 -0500

    [16] Se integra el Boton de PayPal al container Payment exitosamente

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,21 +1,76 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { PayPalButton } from 'react-paypal-button-v2';
+import { Link, useNavigate } from 'react-router-dom';
+import AppContext from '../context/AppContext';
 import '../styles/components/Payment.css';
 
 const Payment = () => {
+  const { state, addNewOrder } = useContext(AppContext);
+  const { cart, buyer } = state;
+  const navigate = useNavigate();
+
+  const paypalOptions = {
+    clientId:'AWR_4IWWA2rer5pJAQ0ZHCgiPn48womazQeFmK185-soCCxt_KdUbDf-ZVSDg0C97K_btw9et6W22ZEL',
+    intent: 'capture',
+    currency: 'USD'
+}
+
+const buttonStyles = {
+    layout: 'horizontal',
+    color: 'gold',
+    shape: 'rect',
+    label: 'paypal'
+}
+
+const handlePaymentSuccess = (data) => {
+    console.log(data);
+    if(data.status === 'COMPLETED') {
+        const newOrder = {
+            buyer,
+            product: cart,
+            payment: data
+        }
+        addNewOrder(newOrder);
+        navigate('/checkout/success');
+    }
+}
+
+const handleSumTotal = () => {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue.price;
+    const sum = cart.reduce(reducer, 0);
+    return sum;
+}
+
   return (
     <div className="Payment">
       <div className="Payment-content">
         <h3>Resumen del pedido:</h3>
+        {cart.map((item) =>(
+          <div className="Payment-item" key={item.title}>
+            <div className="Payment-element">
+              <h4>{item.title}</h4>
+                <span>${' '}{item.price}</span>
+            </div>
+          </div>
+        ))}
         <div className="Payment-button">
-          Boton de Pago con Paypal
+          <PayPalButton 
+            paypalOptions={paypalOptions}
+            buttonStyles={buttonStyles}
+            amount={handleSumTotal()}
+            onSuccess={data => handlePaymentSuccess(data)}
+            onError={error => console.log(error)}
+            onCancel={data => console.log(data)}
+          />
         </div>
       </div>
+
       <div className="Payment-navigator">
-      <Link to="/checkout/information">
-        <button type="text">Regresar a Informacion de Contancto </button>
-      </Link>
-    </div>
+        <Link to="/checkout/information">
+          <button type="text">Regresar a Informacion de Contancto </button>
+        </Link>
+      </div>
+
     </div>
 
   );
